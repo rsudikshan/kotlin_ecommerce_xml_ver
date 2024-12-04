79b904a448b0f161bc6a060f1b4dc8f09558e492
Sudikshan <rsudikshan2@gmail.com>
2024-12-04 20:15:56 +0545

    initial message

---
+++ app/src/main/java/com/sr/kt_ecommerce/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.sr.kt_ecommerce
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity:AppCompatActivity(){
+
+     override fun onCreate(b: Bundle){
+        super.onCreate(b);
+        setContentView(R.layout.main_screen);
+
+
+    }
+
+}
